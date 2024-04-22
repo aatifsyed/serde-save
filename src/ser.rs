1a57d5b209b227c44a873d5cf9e62450ca77d965
Aatif Syed <aatifsyedyp@gmail.com>
2024-04-22 01:57:37 +0100

    wip: protocol error handling

--- src/ser.rs
+++ src/ser.rs
@@ -1,10 +1,16 @@
 use crate::{Save, Variant};
-use core::{fmt, iter};
-use std::{convert::Infallible, marker::PhantomData};
+use core::{convert::Infallible, fmt, marker::PhantomData};
 
 #[derive(Debug)]
 pub struct Error {
     msg: String,
+    protocol: bool,
+}
+
+impl Error {
+    pub fn is_protocol(&self) -> bool {
+        self.protocol
+    }
 }
 
 impl fmt::Display for Error {
@@ -17,6 +23,7 @@ impl serde::ser::Error for Error {
     fn custom<T: fmt::Display>(msg: T) -> Self {
         Self {
             msg: msg.to_string(),
+            protocol: false,
         }
     }
 }
@@ -60,6 +67,7 @@ impl Serializer<ShortCircuit> {
         Self {
             config: Config {
                 is_human_readable: false,
+                protocol_errors: true,
                 _error_discipline: PhantomData,
             },
         }
@@ -71,17 +79,23 @@ impl<E> Serializer<E> {
         self.config.is_human_readable = is_human_readable;
         self
     }
+    pub fn check_for_protocol_errors(mut self, check: bool) -> Self {
+        self.config.protocol_errors = check;
+        self
+    }
     pub fn save_errors(self) -> Serializer<Persist> {
         let Self {
             config:
                 Config {
                     is_human_readable,
+                    protocol_errors,
                     _error_discipline,
                 },
         } = self;
         Serializer {
             config: Config {
                 is_human_readable,
+                protocol_errors,
                 _error_discipline: PhantomData,
             },
         }
@@ -100,7 +114,7 @@ struct Config<E = ShortCircuit> {
     // - jagged key/value pairs in a map
     // - incorrect sequence/tuple lengths
     // - duplicate field names
-    // protocol_errors: bool,
+    protocol_errors: bool,
     _error_discipline: PhantomData<fn() -> E>,
 }
 
@@ -222,12 +236,14 @@ where
         Ok(imp::SerializeSeq {
             config: self.config,
             inner: Vec::with_capacity(len.unwrap_or_default()),
+            expected_len: len,
         })
     }
     fn serialize_tuple(self, len: usize) -> Result<Self::SerializeTuple, Self::Error> {
         Ok(imp::SerializeTuple {
             config: self.config,
             inner: Vec::with_capacity(len),
+            expected_len: len,
         })
     }
     fn serialize_tuple_struct(
@@ -236,6 +252,7 @@ where
         len: usize,
     ) -> Result<Self::SerializeTupleStruct, Self::Error> {
         Ok(imp::SerializeTupleStruct {
+            expected_len: len,
             config: self.config,
             name,
             values: Vec::with_capacity(len),
@@ -249,8 +266,8 @@ where
         len: usize,
     ) -> Result<Self::SerializeTupleVariant, Self::Error> {
         Ok(imp::SerializeTupleVariant {
+            expected_len: len,
             config: self.config,
-
             variant: Variant {
                 name,
                 variant_index,
@@ -263,6 +280,7 @@ where
         let capacity = len.unwrap_or_default();
         Ok(imp::SerializeMap {
             config: self.config,
+            expected_len: len,
             keys: Vec::with_capacity(capacity),
             values: Vec::with_capacity(capacity),
         })
@@ -273,6 +291,7 @@ where
         len: usize,
     ) -> Result<Self::SerializeStruct, Self::Error> {
         Ok(imp::SerializeStruct {
+            expected_len: len,
             config: self.config,
             name,
             fields: Vec::with_capacity(len),
@@ -295,17 +314,44 @@ where
             },
             fields: Vec::with_capacity(len),
             skipped_fields: Vec::new(),
+            expected_len: len,
         })
     }
 }
 
 mod imp {
-    use serde::ser::Error as _;
+    use std::cmp;
 
     use super::*;
 
+    fn check_length<E>(
+        what: &str,
+        config: &Config<E>,
+        expected: usize,
+        pushing: &mut Vec<Save<E::SaveError>>,
+    ) -> Result<(), Error>
+    where
+        E: ErrorDiscipline,
+    {
+        if config.protocol_errors {
+            let actual = pushing.len();
+            if expected != actual {
+                let e = Error {
+                    msg: format!(
+                        "protcol error: expected a {} of length {}, got {}",
+                        what, expected, actual
+                    ),
+                    protocol: true,
+                };
+                pushing.push(E::handle(Err(e))?)
+            }
+        }
+        Ok(())
+    }
+
     pub struct SerializeSeq<E: ErrorDiscipline> {
         pub(super) config: Config<E>,
+        pub(super) expected_len: Option<usize>,
         pub(super) inner: Vec<Save<E::SaveError>>,
     }
     impl<E> serde::ser::SerializeSeq for SerializeSeq<E>
@@ -323,11 +369,15 @@ mod imp {
             }))?);
             Ok(())
         }
-        fn end(self) -> Result<Self::Ok, Self::Error> {
+        fn end(mut self) -> Result<Self::Ok, Self::Error> {
+            if let Some(expected_len) = self.expected_len {
+                check_length("sequence", &self.config, expected_len, &mut self.inner)?;
+            }
             Ok(Save::Seq(self.inner))
         }
     }
     pub struct SerializeTuple<E: ErrorDiscipline> {
+        pub(super) expected_len: usize,
         pub(super) config: Config<E>,
         pub(super) inner: Vec<Save<E::SaveError>>,
     }
@@ -346,11 +396,13 @@ mod imp {
             }))?);
             Ok(())
         }
-        fn end(self) -> Result<Self::Ok, Self::Error> {
+        fn end(mut self) -> Result<Self::Ok, Self::Error> {
+            check_length("tuple", &self.config, self.expected_len, &mut self.inner)?;
             Ok(Save::Tuple(self.inner))
         }
     }
     pub struct SerializeTupleStruct<E: ErrorDiscipline> {
+        pub(super) expected_len: usize,
         pub(super) config: Config<E>,
         pub(super) name: &'static str,
         pub(super) values: Vec<Save<E::SaveError>>,
@@ -371,7 +423,13 @@ mod imp {
             Ok(())
         }
 
-        fn end(self) -> Result<Self::Ok, Self::Error> {
+        fn end(mut self) -> Result<Self::Ok, Self::Error> {
+            check_length(
+                "tuple struct",
+                &self.config,
+                self.expected_len,
+                &mut self.values,
+            )?;
             Ok(Save::TupleStruct {
                 name: self.name,
                 values: self.values,
@@ -379,6 +437,7 @@ mod imp {
         }
     }
     pub struct SerializeTupleVariant<E: ErrorDiscipline> {
+        pub(super) expected_len: usize,
         pub(super) config: Config<E>,
         pub(super) variant: Variant,
         pub(super) values: Vec<Save<E::SaveError>>,
@@ -398,7 +457,14 @@ mod imp {
             }))?);
             Ok(())
         }
-        fn end(self) -> Result<Self::Ok, Self::Error> {
+        fn end(mut self) -> Result<Self::Ok, Self::Error> {
+            check_length(
+                "tuple variant",
+                &self.config,
+                self.expected_len,
+                &mut self.values,
+            )?;
+
             Ok(Save::TupleVariant {
                 variant: self.variant,
                 values: self.values,
@@ -406,6 +472,7 @@ mod imp {
         }
     }
     pub struct SerializeMap<E: ErrorDiscipline> {
+        pub(super) expected_len: Option<usize>,
         pub(super) config: Config<E>,
         pub(super) keys: Vec<Save<E::SaveError>>,
         pub(super) values: Vec<Save<E::SaveError>>,
@@ -435,15 +502,47 @@ mod imp {
             Ok(())
         }
         fn end(self) -> Result<Self::Ok, Self::Error> {
-            match self.keys.len() == self.values.len() {
-                true => Ok(Save::Map(iter::zip(self.keys, self.values).collect())),
-                false => Err(Error::custom(
-                    "number of keys and values in map doesn't match",
-                )),
+            let n_keys = self.keys.len();
+            let n_values = self.values.len();
+            let mut map = Vec::with_capacity(cmp::max(n_keys, n_values));
+            let mut keys = self.keys.into_iter();
+            let mut values = self.values.into_iter();
+            loop {
+                let e = || Error {
+                    msg: format!(
+                        "protocol error: map has {} keys and {} values",
+                        n_keys, n_values
+                    ),
+                    protocol: true,
+                };
+                match (keys.next(), values.next()) {
+                    (None, None) => {
+                        if let Some(expected) = self.expected_len {
+                            if self.config.protocol_errors && expected != map.len() {
+                                let e = || Error {
+                                    msg: format!(
+                                        "protcol error: expected a map of length {}, got {}",
+                                        expected,
+                                        map.len()
+                                    ),
+                                    protocol: true,
+                                };
+                                map.push((E::handle(Err(e()))?, E::handle(Err(e()))?))
+                            }
+                        }
+                        return Ok(Save::Map(map));
+                    }
+                    (Some(key), Some(value)) => map.push((key, value)),
+                    (None, Some(value)) => map.push((E::handle(Err(e()))?, value)),
+                    (Some(key), None) => map.push((key, E::handle(Err(e()))?)),
+                }
             }
         }
     }
     pub struct SerializeStruct<E: ErrorDiscipline> {
+        // TODO(aatifsyed): handle mismatch and field name duplications
+        #[allow(unused)]
+        pub(super) expected_len: usize,
         pub(super) config: Config<E>,
         pub(super) name: &'static str,
         pub(super) fields: Vec<(&'static str, Save<E::SaveError>)>,
@@ -481,6 +580,9 @@ mod imp {
         }
     }
     pub struct SerializeStructVariant<E: ErrorDiscipline> {
+        // TODO(aatifsyed): handle mismatch and field name duplications
+        #[allow(unused)]
+        pub(super) expected_len: usize,
         pub(super) config: Config<E>,
         pub(super) variant: Variant,
         pub(super) fields: Vec<(&'static str, Save<E::SaveError>)>,
