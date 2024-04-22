5362bcf5d7fd3b62cb534fb178a235948b6eb857
Aatif Syed <aatifsyedyp@gmail.com>
2024-04-22 03:16:06 +0100

    feat: impl Deserialize for Save

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 //! The most complete serialization tree for [`serde`].
 
-use core::fmt;
-use std::convert::Infallible;
+use core::{convert::Infallible, fmt};
+use std::iter;
 
 use serde::{
     ser::{
         Error as _, SerializeMap as _, SerializeStruct as _, SerializeStructVariant as _,
         SerializeTuple as _, SerializeTupleStruct as _, SerializeTupleVariant as _,
     },
-    Serialize,
+    Deserialize, Serialize,
 };
 
 pub mod ser;
@@ -211,5 +211,134 @@ where
             }
             Save::Error(e) => Err(S::Error::custom(e)),
         }
+    }
+}
+
+impl<'de> Deserialize<'de> for Save {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        struct Visitor;
+
+        macro_rules! simple {
+            ($($fn:ident($ty:ty) -> $variant:ident);* $(;)?) => {
+                $(
+                    fn $fn<E: serde::de::Error>(self, v: $ty) -> Result<Self::Value, E> {
+                        Ok(Save::$variant(v))
+                    }
+                )*
+            };
+        }
+        impl<'de> serde::de::Visitor<'de> for Visitor {
+            type Value = Save;
+
+            fn expecting(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                f.write_str("a `Save`-able type")
+            }
+
+            simple! {
+                visit_bool(bool) -> Bool;
+                visit_i8(i8) -> I8;
+                visit_i16(i16) -> I16;
+                visit_i32(i32) -> I32;
+                visit_i64(i64) -> I64;
+                visit_i128(i128) -> I128;
+                visit_u8(u8) -> U8;
+                visit_u16(u16) -> U16;
+                visit_u32(u32) -> U32;
+                visit_u64(u64) -> U64;
+                visit_u128(u128) -> U128;
+                visit_f32(f32) -> F32;
+                visit_f64(f64) -> F64;
+                visit_char(char) -> Char;
+                visit_string(String) -> String;
+                visit_byte_buf(Vec<u8>) -> ByteArray;
+            }
+
+            fn visit_str<E: serde::de::Error>(self, v: &str) -> Result<Self::Value, E> {
+                Ok(Save::String(v.into()))
+            }
+
+            fn visit_borrowed_str<E: serde::de::Error>(
+                self,
+                v: &'de str,
+            ) -> Result<Self::Value, E> {
+                Ok(Save::String(v.into()))
+            }
+
+            fn visit_bytes<E: serde::de::Error>(self, v: &[u8]) -> Result<Self::Value, E> {
+                Ok(Save::ByteArray(v.into()))
+            }
+
+            fn visit_borrowed_bytes<E: serde::de::Error>(
+                self,
+                v: &'de [u8],
+            ) -> Result<Self::Value, E> {
+                Ok(Save::ByteArray(v.into()))
+            }
+
+            fn visit_none<E: serde::de::Error>(self) -> Result<Self::Value, E> {
+                Ok(Save::Option(None))
+            }
+
+            fn visit_some<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                Ok(Save::Option(Some(Box::new(
+                    deserializer.deserialize_any(Self)?,
+                ))))
+            }
+
+            fn visit_unit<E: serde::de::Error>(self) -> Result<Self::Value, E> {
+                Ok(Save::Unit)
+            }
+
+            fn visit_newtype_struct<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                let _ = deserializer;
+                Err(serde::de::Error::invalid_type(
+                    serde::de::Unexpected::NewtypeStruct,
+                    &self,
+                ))
+            }
+
+            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+            where
+                A: serde::de::SeqAccess<'de>,
+            {
+                Ok(Save::Seq(
+                    iter::from_fn(|| seq.next_element().transpose())
+                        .fuse()
+                        .collect::<Result<_, _>>()?,
+                ))
+            }
+
+            fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+            where
+                A: serde::de::MapAccess<'de>,
+            {
+                Ok(Save::Map(
+                    iter::from_fn(|| map.next_entry().transpose())
+                        .fuse()
+                        .collect::<Result<_, _>>()?,
+                ))
+            }
+
+            fn visit_enum<A>(self, data: A) -> Result<Self::Value, A::Error>
+            where
+                A: serde::de::EnumAccess<'de>,
+            {
+                let _ = data;
+                Err(serde::de::Error::invalid_type(
+                    serde::de::Unexpected::Enum,
+                    &self,
+                ))
+            }
+        }
+        deserializer.deserialize_any(Visitor)
     }
 }
